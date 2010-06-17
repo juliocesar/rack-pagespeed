5d37e14448f8d1c67b621835d9bba816b1f20d03
Julio Cesar Ody <julioody@gmail.com>
2010-06-17 16:21:28 +1000

    some specs and code for DatabaseStore

--- lib/rack/bundle/database_store.rb
+++ lib/rack/bundle/database_store.rb
@@ -1,5 +1,22 @@
 require 'sequel'
 
 class Rack::Bundle::DatabaseStore
+  attr_accessor :db, :bundles
   
+  def initialize url = ENV['DATABASE_URL']
+    @db = Sequel.connect(url)
+    @bundles = []
+    create_table!
+  end
+  
+  def find_bundle_by_hash hash
+  end
+    
+  private
+  def create_table!
+    @db.create_table! 'rack_bundle' do
+      primary_key :hash
+      Text        :contents
+    end
+  end
 end
--- spec/store/database_store_spec.rb
+++ spec/store/database_store_spec.rb
@@ -1,6 +1,21 @@
 require File.join(File.dirname(__FILE__), '..', 'spec_helper')
 
-describe Rack::Bundle::DatabaseStore do
-  # Getting onto it
-  it 'does the same FileSystemStore does except it persists to a database using Sequel'
+describe Rack::Bundle::DatabaseStore do  
+  context 'initializing' do
+    before do
+      ENV['DATABASE_URL'] = 'sqlite:///tmp/foo.db'
+      @db_store = Rack::Bundle::DatabaseStore.new
+    end
+    
+    it 'looks for a DATABASE_URL environment variable when a database URL is not specified' do
+      @db_store.db.url.should == 'sqlite://tmp/foo.db'
+    end
+    it 'takes a database url as a parameter' do
+      db_store = Rack::Bundle::DatabaseStore.new 'sqlite:///tmp/bar.db'
+      db_store.db.url.should == 'sqlite://tmp/bar.db'
+    end
+    it 'creates a table to store bundles in' do
+      @db_store.db.tables.should include(:rack_bundle)
+    end
+  end
 end
