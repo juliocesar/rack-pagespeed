e84d9a628cdf2ed6c357abce372bd0022bb38012
Julio Cesar Ody <julioody@gmail.com>
2010-12-15 09:54:21 +1100

    ahem, broken specs

--- spec/filters/inline_javascript_spec.rb
+++ spec/filters/inline_javascript_spec.rb
@@ -8,8 +8,6 @@ describe 'the inline_javascript filter' do
   context "#execute!" do
     before :each do
       @inliner = Rack::PageSpeed::Filters::InlineJavaScript.new :public => FIXTURES_PATH
-      @document = FIXTURES.complex
-      @inliner.execute! @document
     end
 
     it "returns false if there are no script nodes in the document" do
@@ -17,13 +15,13 @@ describe 'the inline_javascript filter' do
     end
 
     it 'inlines JavaScripts that are smaller than 2kb in size by default' do
-      @document.at_css('script:not([src])').content.should == fixture('mylib.js')
+      @inliner.execute! FIXTURES.complex
+      FIXTURES.complex.at_css('script:not([src])').content.should == fixture('mylib.js')
     end
 
     it 'does stuff, and the maximum size threshold is controlled via :max_size' do
       inliner = Rack::PageSpeed::Filters::InlineJavaScript.new :max_size => 999999, :public => FIXTURES_PATH
-      inliner.execute! @document
-      @document.css('script:not([src])').count.should == 2
+      expect { inliner.execute! FIXTURES.complex }.to change { FIXTURES.complex.css('script:not([src])').count }.by(4)
     end
   end
 end
