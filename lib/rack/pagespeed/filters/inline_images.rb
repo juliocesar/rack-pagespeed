6c802455300fac0ecf9bdf611afcf231ab7a7210
Julio Cesar Ody <julioody@gmail.com>
2011-01-10 16:58:25 +1100

    bump, InlineImages now clones a node instead of creating a new one in order to keep attributes around

--- lib/rack/pagespeed/filters/inline_images.rb
+++ lib/rack/pagespeed/filters/inline_images.rb
@@ -7,7 +7,7 @@ class Rack::PageSpeed::Filters::InlineImages < Rack::PageSpeed::Filter
     nodes.each do |node|
       file = file_for node
       next if !file or file.stat.size > (@options[:max_size] or 1024)
-      img = Nokogiri::XML::Node.new 'img', document
+      img = node.clone
       img['src'] = "data:#{Rack::Mime.mime_type(File.extname(file.path))};base64,#{[file.read].pack('m')}"
       img['alt'] = node['alt'] if node['alt']
       node.before img
